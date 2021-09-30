03030d8c11debad73e0605a63a8e688c7c09b66b
alexey <alexey.n@smplt.net>
2021-09-30 14:52:06 +0300

    start nosql client

--- src/Service.Liquidity.InternalWallets/ApplicationLifetimeManager.cs
+++ src/Service.Liquidity.InternalWallets/ApplicationLifetimeManager.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MyJetWallet.Sdk.NoSql;
 using MyJetWallet.Sdk.Service;
 
 namespace Service.Liquidity.InternalWallets
@@ -7,21 +8,28 @@ namespace Service.Liquidity.InternalWallets
     public class ApplicationLifetimeManager : ApplicationLifetimeManagerBase
     {
         private readonly ILogger<ApplicationLifetimeManager> _logger;
+        private readonly MyNoSqlClientLifeTime _noSqlClientLifeTime;
 
-        public ApplicationLifetimeManager(IHostApplicationLifetime appLifetime, ILogger<ApplicationLifetimeManager> logger)
+        public ApplicationLifetimeManager(
+        IHostApplicationLifetime appLifetime, 
+        ILogger<ApplicationLifetimeManager> logger,
+        MyNoSqlClientLifeTime noSqlClientLifeTime)
             : base(appLifetime)
         {
             _logger = logger;
+            _noSqlClientLifeTime = noSqlClientLifeTime;
         }
 
         protected override void OnStarted()
         {
             _logger.LogInformation("OnStarted has been called.");
+            _noSqlClientLifeTime.Start();
         }
 
         protected override void OnStopping()
         {
             _logger.LogInformation("OnStopping has been called.");
+            _noSqlClientLifeTime.Stop();
         }
 
         protected override void OnStopped()
