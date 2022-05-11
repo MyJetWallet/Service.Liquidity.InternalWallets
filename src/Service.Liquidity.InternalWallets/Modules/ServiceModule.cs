4b961f2b5c2dce9ca9bee2bddf15fc1a8c66183c
Maxim Gorin <maksim.g@smplt.net>
2022-05-11 14:45:02 +0300

    Fix build after nosql sdk update

--- src/Service.Liquidity.InternalWallets/Modules/ServiceModule.cs
+++ src/Service.Liquidity.InternalWallets/Modules/ServiceModule.cs
@@ -14,7 +14,7 @@ namespace Service.Liquidity.InternalWallets.Modules
         protected override void Load(ContainerBuilder builder)
         {
             
-            var myNoSqlClient = builder.CreateNoSqlClient(Program.ReloadedSettings(e => e.MyNoSqlReaderHostPort));
+            var myNoSqlClient = builder.CreateNoSqlClient(Program.Settings.MyNoSqlReaderHostPort, Program.LogFactory);
             
             builder
                 .RegisterType<LpWalletManager>()
